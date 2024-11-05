c15bc6815f73520c491677cc693c9157ea285e17
brandanblevins <brandan.blevins@gmail.com>
2024-11-05 12:11:44 -0700

    Add next step in treasure island game

--- Day 3/treasureislandgame.py
+++ Day 3/treasureislandgame.py
@@ -3,7 +3,12 @@ print("Welcome to Treasure Island! Your mission is to find the treasure.")
 left_or_right = str(input("Do you want to take the left path or the right path? Type left or right: "))
 
 if left_or_right == "left":
-    swim_or_wait = str(input("Good choice! Now, do you want to swim across the lake or wait here? Type swim or wait:"))
+    swim_or_wait = str(input("Good choice! Now, do you want to swim across the lake or wait here? Type swim or wait: "))
 else:
     print("You fell into a hole! GAME OVER!")
+
+if swim_or_wait == "wait":
+    choose_door = str(input("Another good choice! Very wise. Now, which door do you want to take? Type red, blue, yellow: "))
+else:
+    print("You've been attacked by a giant killer trout! GAME OVER")
 
