1f02cae1cb6a5c49fc72bf01ea0f0421b79c2e7e
brandanblevins <brandan.blevins@gmail.com>
2024-11-05 12:08:07 -0700

    Fix formatting

--- Day 3/treasureislandgame.py
+++ Day 3/treasureislandgame.py
@@ -2,7 +2,7 @@ print("Welcome to Treasure Island! Your mission is to find the treasure.")
 
 left_or_right = str(input("Do you want to take the left path or the right path? Type left or right: "))
 
-if left_or_right == left
+if left_or_right == left:
     swim_or_wait = str(input("Good choice! Now, do you want to swim across the lake or wait here? Type swim or wait:"))
 else:
     print("You fell into a hole! GAME OVER!")
