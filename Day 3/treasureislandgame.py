45ddb66e9e459651ecdc4979d4e2059543ddbd2f
brandanblevins <brandan.blevins@gmail.com>
2024-11-05 12:20:40 -0700

    Add more ascii art

--- Day 3/treasureislandgame.py
+++ Day 3/treasureislandgame.py
@@ -13,9 +13,17 @@ else:
     print("You've been attacked by a giant killer trout! GAME OVER")
 
 if choose_door == "red":
-    print("You've been roasted by the flaming fire of doom! GAME OVER")
+    print("You've been roasted by the flaming fire of doom! GAME OVER" '''
+                   (  .      )
+           )           (              )
+                 .  '   .   '  .  '  .
+        (    , )       (.   )  (   ',    )
+         .' ) ( . )    ,  ( ,     )   ( .
+      ). , ( .   (  ) ( , ')  .' (  ,    )
+     (_,) . ), ) _) _,')  (, ) '. )  ,. (' ) ''')
 elif choose_door == "blue":
-    print("You've been eaten by the beasts of the dark forest! GAME OVER" '''                                             ___.-----.______
+    print("You've been eaten by the beasts of the dark forest! GAME OVER" '''
+                                            ___.-----.______
                                    ___.-----'::::::::::::::::`---.___
                 _.--._            (:::;,-----'~~~~~`----::::::::::.. `-.
    _          .'_---. `--.__       `~~'                 `~`--.:::::`..  `..
@@ -36,6 +44,53 @@ elif choose_door == "blue":
   ~~~~~~~~
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ''')
 elif choose_door == "yellow":
-    print("You are the wisest player yet! Congratulations on successfully completing your noble quest!")
+    print("You are the wisest player yet! Congratulations on successfully completing your noble quest!" '''
+                  *
+             / \
+            /___\
+           ( o o )            * *
+           )  L  (           /   * *
+   ________()(-)()________  /     * * *
+ E\| _____ )()()() ______ |/B     * * *
+   |/      ()()()(       \|      * * * *
+           | )() |
+           /     \
+          / *  *  \
+         /   *  *  \
+        / *_  *  _  \   ''')
 else:
-    print("You don't understand the game! GAME OVER")
+    print("You don't understand the game! GAME OVER" '''
+         _______________,,.
+    /_____________.;;'/|
+   |"____  _______;;;]/
+   | || //'         ;
+   | ||//'          ;
+   | |//'           ;
+   |  /'            $
+   | ||             $
+   | ||             $
+   | ||            ,^.
+   | ||            | |
+   | ||            `.'
+   | || 
+   | || 
+   | || 
+   | || 
+   | ||   _________________________   
+   | ||  /                        4
+   | || /                        /| 
+   | ||/           _____        / /
+   | ||           /|___/       / /|
+   | ||          / |  /       / /||
+   |_|/         / cj /       / / ||
+  /             """""       / /| ||
+ /                         / / | ||
+/                         / /  | ||
+""""""""""""""""""""""""""|/   | ||
+__________________________f|   | |
+| ||                    | ||           
+| ||                    | ||    
+| ||                    | ||
+| ||                    | ||
+| ||                    | ||
+| |                     | | ''')
